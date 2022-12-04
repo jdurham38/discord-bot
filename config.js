b0dd5a2d808c1797f5d5949950d7987ca523864e
jdurham38 <60802210+jdurham38@users.noreply.github.com>
2022-12-03 20:39:01 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
     app: {
-        token: 'MTA0ODM0MzE3MjU5OTc5MTY4Ng.GqQNiJ.02_gXWZBDP35m_sCFI_cyHXnz8skH6jTG7wbME',
+        token: 'xxxxxxx',
         playing: 'C, R, B',
         global: true,
-        guild: '353309519486386176'
+        guild: 'xxxxxxx'
     },
 
     opt: {
